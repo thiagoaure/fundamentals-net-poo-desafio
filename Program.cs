e92a04c5bc58e3e30d49b94ad4647dcb5c8a51b9
Thiago Aureliano <thiagoh544@gmail.com>
2023-09-14 16:53:02 -0300

    feat: initial

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using DesafioPOO.Models;
+
+Smartphone nokia = new Nokia("3599999999", "Modelo N3", "111000", "16");
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("DIO");
+
+Smartphone iphone = new Iphone("11888888", "Modelo 14", "000111", "128");
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
